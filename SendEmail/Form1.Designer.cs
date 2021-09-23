570880b19e48f3d1d0ae8d9280e44378e2629170
Adri Oliveira <adrianne.oliveira@capgemini.com>
2021-09-23 16:32:44 -0300

    First Commit

---
+++ SendEmail/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace SendEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txbFrom = new System.Windows.Forms.TextBox();
+            this.txbTo = new System.Windows.Forms.TextBox();
+            this.txbSubject = new System.Windows.Forms.TextBox();
+            this.txbMessage = new System.Windows.Forms.TextBox();
+            this.lblAttach = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FROM:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TO:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SUBJECT:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MESSAGE:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ATTACH:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSend.Location = new System.Drawing.Point(405, 320);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(179, 43);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txbFrom
+            // 
+            this.txbFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbFrom.Location = new System.Drawing.Point(70, 28);
+            this.txbFrom.Name = "txbFrom";
+            this.txbFrom.Size = new System.Drawing.Size(514, 20);
+            this.txbFrom.TabIndex = 6;
+            // 
+            // txbTo
+            // 
+            this.txbTo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbTo.Location = new System.Drawing.Point(70, 69);
+            this.txbTo.Name = "txbTo";
+            this.txbTo.Size = new System.Drawing.Size(514, 20);
+            this.txbTo.TabIndex = 7;
+            // 
+            // txbSubject
+            // 
+            this.txbSubject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbSubject.Location = new System.Drawing.Point(70, 108);
+            this.txbSubject.Name = "txbSubject";
+            this.txbSubject.Size = new System.Drawing.Size(514, 20);
+            this.txbSubject.TabIndex = 8;
+            // 
+            // txbMessage
+            // 
+            this.txbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbMessage.Location = new System.Drawing.Point(70, 158);
+            this.txbMessage.Multiline = true;
+            this.txbMessage.Name = "txbMessage";
+            this.txbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbMessage.Size = new System.Drawing.Size(514, 90);
+            this.txbMessage.TabIndex = 9;
+            // 
+            // lblAttach
+            // 
+            this.lblAttach.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAttach.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAttach.Location = new System.Drawing.Point(70, 261);
+            this.lblAttach.Name = "lblAttach";
+            this.lblAttach.Size = new System.Drawing.Size(514, 23);
+            this.lblAttach.TabIndex = 10;
+            this.lblAttach.Click += new System.EventHandler(this.lblAttach_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(472, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Click to Add a file/files";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(199, 320);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(179, 43);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(612, 375);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblAttach);
+            this.Controls.Add(this.txbMessage);
+            this.Controls.Add(this.txbSubject);
+            this.Controls.Add(this.txbTo);
+            this.Controls.Add(this.txbFrom);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Send Email";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txbFrom;
+        private System.Windows.Forms.TextBox txbTo;
+        private System.Windows.Forms.TextBox txbSubject;
+        private System.Windows.Forms.TextBox txbMessage;
+        private System.Windows.Forms.Label lblAttach;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ SendEmail/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace SendEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var emailFrom = ConfigurationManager.AppSettings["EmailFrom"];
+            var team = ConfigurationManager.AppSettings["Team"];
+            var subject = ConfigurationManager.AppSettings["Subject"];
+            txbFrom.Text = emailFrom;
+            txbTo.Text = team;
+            txbSubject.Text = subject;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            EnviarEmail();
+        }
+
+        private void EnviarEmail()
+        {
+            var smtpCredential = ConfigurationManager.AppSettings["Smtp"];
+            var user = ConfigurationManager.AppSettings["User"];
+            var password = ConfigurationManager.AppSettings["Password"];
+            var emailFrom = ConfigurationManager.AppSettings["EmailFrom"];
+            var emailTo = ConfigurationManager.AppSettings["ListEmails"];
+
+            try
+            {
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    using (MailMessage email = new MailMessage())
+                    {
+                        //Server SMTP
+                        smtp.Host = smtpCredential;
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new NetworkCredential(user, password);
+                        smtp.Port = 587;
+                        smtp.EnableSsl = false;
+
+                        //Email
+                        email.From = new MailAddress(emailFrom);
+                        foreach (var address in emailTo.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            email.To.Add(address);
+                        }
+
+                        email.Subject = txbSubject.Text;
+                        email.IsBodyHtml = false;
+                        email.Body = txbMessage.Text;
+
+                        //Attachs
+                        if (lblAttach.Text != "")
+                        {
+                            var attach = lblAttach.Text.ToString().Split(';');
+                            for (int i = 0; i < attach.Length; i++)
+                                email.Attachments.Add(new Attachment(attach[i]));
+                        }
+
+                        //Send Email
+                        smtp.Send(email);
+                    }
+                }
+
+                MessageBox.Show("Email Enviado!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+
+        private void lblAttach_Click(object sender, EventArgs e)
+        {
+            var attach = new OpenFileDialog();
+
+            attach.Multiselect = true;
+            attach.Title = "Attach files";
+            if (attach.ShowDialog() == DialogResult.OK)
+                for (int i = 0; i < attach.FileNames.Length;i++)
+                    if (i == 0)
+                        lblAttach.Text = attach.FileNames[i];
+                    else
+                        lblAttach.Text = lblAttach.Text + "; " + attach.FileNames[i];
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txbMessage.Clear();
+            txbSubject.Clear();
+        }
+    }
+}
